aeb9366aebd10fde45b44d5f8f53d9bd57f4f574
meekranjer <logicism81@gmail.com>
2023-02-16 21:07:03 -0500

    Finished GUI

---
+++ job_service/gui.py
@@ -0,0 +1,82 @@
+import tkinter as tk
+import time
+from main import auto_book
+
+seed = "https://reservation.frontdesksuite.ca/rcfs/nepeansportsplex/Home/Index?Culture=en&PageId=b0d362a1-ba36-42ae-b1e0-feefaf43fe4c&ShouldStartReserveTimeFlow=False&ButtonId=00000000-0000-0000-0000-000000000000"
+
+
+def submit_form():
+    submit_button.config(text="Stop")
+    status_bar.config(text="Running", fg="orange")
+    root.update()
+
+    court_name = court_entry.get()
+    time_aria = time_entry.get()
+    telephone = phone_entry.get()
+    email = email_entry.get()
+    name = name_entry.get()
+    is_test_run = test_run_var.get()
+
+    res = auto_book(seed, court_name, time_aria,
+                    telephone, email, name, is_test_run)
+
+    submit_button.config(text="Start")
+    status_bar.config(text="Failed", fg="red")
+
+    if (res):
+        status_bar.config(text="Completed", fg="green")
+
+    root.update()
+
+
+# Create the main window
+root = tk.Tk()
+root.title("Nepean Bookinator 3000")
+
+# Create the form fields
+court_label = tk.Label(root, text="Court")
+court_label.pack()
+court_entry = tk.Entry(root, width=30, justify="center")
+court_entry.insert(0, "Badminton")
+court_entry.pack()
+
+time_label = tk.Label(root, text="Time")
+time_label.pack()
+time_entry = tk.Entry(root, width=30, justify="center")
+time_entry.insert(0, "6:30 PM Friday February 17, 2023")
+time_entry.pack()
+
+phone_label = tk.Label(root, text="Phone Number")
+phone_label.pack()
+phone_entry = tk.Entry(root, width=30, justify="center")
+# phone_entry.insert(0, "6138029384")
+phone_entry.pack()
+
+email_label = tk.Label(root, text="Email")
+email_label.pack()
+email_entry = tk.Entry(root, width=30, justify="center")
+# email_entry.insert(0, "contact@jamesyap.org")
+email_entry.pack()
+
+name_label = tk.Label(root, text="Name")
+name_label.pack()
+name_entry = tk.Entry(root, width=30, justify="center")
+# name_entry.insert(0, "James")
+name_entry.pack()
+
+test_run_var = tk.BooleanVar()
+test_run_checkbox = tk.Checkbutton(
+    root, text="Test Run", variable=test_run_var)
+test_run_checkbox.pack()
+
+submit_button = tk.Button(root, text="Start", command=submit_form)
+submit_button.pack()
+
+status_bar = tk.Label(root, text="Ready", fg="gray")
+status_bar.pack()
+
+footer = tk.Label(root, text="contact@jamesyap.org", fg="lightblue")
+footer.pack()
+
+
+root.mainloop()
--- job_service/main.py
+++ job_service/main.py
@@ -14,55 +14,57 @@ import sys
 headless = True
 retry_rate = 0.01
 
-seed = sys.argv[1]
-court_name = sys.argv[2]
-time_aria = sys.argv[3]
-telephone = sys.argv[4]
-email = sys.argv[5]
-name = sys.argv[6]
-env = sys.argv[7]
+# seed = sys.argv[1]
+# court_name = sys.argv[2]
+# time_aria = sys.argv[3]
+# telephone = sys.argv[4]
+# email = sys.argv[5]
+# name = sys.argv[6]
+# env = sys.argv[7]
 
-try:
-    options = Options()
-    if (headless):
-        options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(
-        ChromeDriverManager().install(), options=options)
 
-    driver.get(seed)
+def auto_book(seed, court_name, time_aria, telephone, email, name, is_test_run):
+    try:
+        options = Options()
+        if (headless):
+            options.add_argument('--headless')
+            options.add_argument('--disable-gpu')
+        driver = webdriver.Chrome(
+            ChromeDriverManager().install(), options=options)
 
-    court_name_xpath = f"//div[@class='content' and text()='{court_name}']"
-    driver.find_element_by_xpath(court_name_xpath).click()
+        driver.get(seed)
 
-    while True:
-        try:
-            element = driver.find_element_by_xpath(
-                f"//a[@aria-label='{time_aria}']")
-        except:
-            # print("Element not found, trying again...")
-            driver.refresh()
-            time.sleep(retry_rate)
-            continue
-        break
+        court_name_xpath = f"//div[@class='content' and text()='{court_name}']"
+        driver.find_element_by_xpath(court_name_xpath).click()
 
-    driver.execute_script("arguments[0].click();", element)
+        while True:
+            try:
+                element = driver.find_element_by_xpath(
+                    f"//a[@aria-label='{time_aria}']")
+            except:
+                print("Element not found, trying again...")
+                driver.refresh()
+                time.sleep(retry_rate)
+                continue
+            break
 
-    driver.find_element_by_id('telephone').send_keys(telephone)
-    driver.find_element_by_id('email').send_keys(email)
-    driver.find_element_by_xpath(
-        "//input[starts-with(@id,'field')][@type='text']").send_keys(name)
-    # driver.find_element_by_id('field5970').send_keys(name)
+        driver.execute_script("arguments[0].click();", element)
 
-    driver.find_element_by_id('submit-btn').click()
+        driver.find_element_by_id('telephone').send_keys(telephone)
+        driver.find_element_by_id('email').send_keys(email)
+        driver.find_element_by_xpath(
+            "//input[starts-with(@id,'field')][@type='text']").send_keys(name)
+        # driver.find_element_by_id('field5970').send_keys(name)
 
-    if (env == 'prod'):
         driver.find_element_by_id('submit-btn').click()
 
-    time.sleep(5)
-    driver.quit()
-    print(f"Successfully booked {court_name} at {time_aria} for {name}")
-except Exception as e:
-    print(e)
+        if (not is_test_run):
+            driver.find_element_by_id('submit-btn').click()
 
-sys.stdout.flush()
+        time.sleep(5)
+        driver.quit()
+        print(f"Successfully booked {court_name} at {time_aria} for {name}")
+        return True
+        # sys.stdout.flush()
+    except:
+        return False
